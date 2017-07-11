c56b446b5ef1403eb59f1948a292df55d0e3777c
Unknown <samuelnoll96@gmail.com>
2017-07-11 15:06:16 -0300

    Adding metadata to all files

--- src/main.py
+++ src/main.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python
+
+"""FloripaSat-GRS: The ground station software of the FloripaSat project."""
+
+"""This file runs the software"""
+
+#
+#  main.py
+#  
+#  Copyright (C) 2017, Federal University of Santa Catarina.
+#  
+#  This file is part of FloripaSat-GRS.
+#
+#  FloripaSat-GRS is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  FloripaSat-GRS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with FloripaSat-GRS. If not, see <http://www.gnu.org/licenses/>.
+#  
+#
+
+__author__      = "Samuel Eduardo Noll"
+__copyright__   = "Copyright (C) 2017, Federal University of Santa Catarina"
+__credits__     = ["Fabricio Silva","Gabriel Mariano Marcelino","Samuel Eduardo Noll"]
+__license__     = "GPL3"
+__version__     = "1.0-dev"
+__maintainer__  = "Samuel Eduardo Noll"
+__email__       = "samuelnoll96@gmail.com"
+__status__      = "Prototype"
+
 from PySide import QtCore, QtGui
 from controlMainWindow import ControlMainWindow
 import sys
