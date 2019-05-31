802e4d2340cb77a38cbb4a5129a5bf5f21a87edf
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-05-31 00:08:03 -0300

    Fixing the log messages when receiving a new packet

--- src/version.h
+++ src/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.5.13
+ * \version 0.5.14
  * 
  * \date 13/02/2019
  * 
@@ -37,7 +37,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define GRS_SOFTWARE_VERSION            "0.5.13"
+#define GRS_SOFTWARE_VERSION            "0.5.14"
 
 #endif // VERSION_H_
 
